238cde7e0456569ac72441a62a0856db3359f0d3
Unity Technologies <@unity>
2019-12-11 00:00:00 +0000

    com.unity.platforms.desktop@0.1.8-preview ## [0.1.8-preview] - 2019-12-11
    ### Changed
    * Bump com.unity.platforms version to 0.1.8-preview
    
    ### Added
    * Added Build components - BuildArtifactDesktop, BuildStepProduceDesktopArtifacts, RunInstanceDesktop, RunStepDesktop
    * Added Desktop pipeline

---
+++ Editor/Unity.Platforms.Desktop.Build/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("Unity.Platforms.macOS.Build")]
+[assembly: InternalsVisibleTo("Unity.Platforms.Windows.Build")]
+[assembly: InternalsVisibleTo("Unity.Platforms.Linux.Build")]
---
+++ Editor/Unity.Platforms.Desktop.Build/MenuItemDesktopBuildSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEditor;
+using Unity.Build;
+using Unity.Platforms.Build;
+using BuildPipeline = Unity.Build.BuildPipeline;
+
+namespace Unity.Platforms.Desktop.Build
+{
+    internal static class MenuItemDesktopBuildSettings
+    {
+        internal static void CreateNewBuildSettingsAssetClassic(string pipelinePath)
+        {
+            var pipeline = AssetDatabase.LoadAssetAtPath<BuildPipeline>(pipelinePath);
+            Selection.activeObject = MenuItemBuildSettings.CreateNewBuildSettingsAsset("Classic", new ClassicBuildProfile { Pipeline = pipeline });
+        }
+    }
+}
