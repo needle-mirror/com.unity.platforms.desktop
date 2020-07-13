986bfce25953cf375cbc7965047fb375f3c5aa98
Unity Technologies <@unity>
2020-07-13 00:00:00 +0000

    com.unity.platforms.desktop@0.7.0-preview.2 ## [0.7.0] - 2020-07-13 ### Changed - Update platforms packages to 0.7.0-preview
    ## [0.6.0] - 2020-07-07
    
    ### Changed
    - Updated `com.unity.platforms` package dependency.

--- Runtime/Unity.Platforms.RunLoop.Desktop/RunLoopImpl.Desktop.cs
+++ Runtime/Unity.Platforms.RunLoop.Desktop/RunLoopImpl.Desktop.cs
@@ -1,13 +1,20 @@
 ﻿#if ((UNITY_WINDOWS || UNITY_MACOSX || UNITY_LINUX || UNITY_EDITOR))
+using System.Runtime.InteropServices;
+
 namespace Unity.Platforms
 {
     public class RunLoopImpl
     {
+        [DllImport("lib_unity_platforms_runloop")]
+        private static extern double GetSecondsMonotonic();
+
         public static void EnterMainLoop(RunLoop.RunLoopDelegate runLoopDelegate)
         {
             while (true)
             {
-                if (runLoopDelegate() == false)
+                double timestampInSeconds = GetSecondsMonotonic();
+
+                if (runLoopDelegate(timestampInSeconds) == false)
                     break;
             }
         }
